eb3885e8db967a99b8728fa33c226b150db2d134
nonkr <nonkr@hotmail.com>
2017-12-02 23:13:04 +0800

    add #include <unistd.h>

--- pthread/c/hello_cond.c
+++ pthread/c/hello_cond.c
@@ -14,6 +14,7 @@
 ******************************************************************************/
 #include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 #define NUM_THREADS  3
