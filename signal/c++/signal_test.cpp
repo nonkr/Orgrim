a73ec3bb5d094e13e83069c00d2df05c63f24316
nonkr <nonkr@hotmail.com>
2017-12-28 15:13:34 +0800

    add samples about c++ signal

---
+++ signal/c++/signal_test.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2017, Billie Soong <nonkr@hotmail.com>
+ * All rights reserved.
+ *
+ * This file is under GPL, see LICENSE for details.
+ *
+ * Author: Billie Soong <nonkr@hotmail.com>
+ * Datetime: 2017/12/28 15:07
+ *
+ */
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <iostream>
+#include <csignal>
+#include <unistd.h>
+
+using namespace std;
+
+void signalHandler(int signum)
+{
+    cout << "Interrupt signal (" << signum << ") received.\n";
+
+    // 清理并关闭
+    // 终止程序
+
+    exit(signum);
+}
+
+int main()
+{
+    // 注册信号 SIGINT 和信号处理程序
+    signal(SIGINT, signalHandler);
+
+    while (1)
+    {
+        cout << "Going to sleep...." << endl;
+        sleep(1);
+    }
+
+    return 0;
+}
