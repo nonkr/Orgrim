8741ea94b118cf8946b477ae760de4d2b3c44bf6
nonkr <nonkr@hotmail.com>
2018-01-05 00:03:01 +0800

    add sample about how to use gnu readline

---
+++ tools/readline/c/rl.c
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2018, Billie Soong <nonkr@hotmail.com>
+ * All rights reserved.
+ *
+ * This file is under GPL, see LICENSE for details.
+ *
+ * Author: Billie Soong <nonkr@hotmail.com>
+ * Datetime: 2018/1/4 23:54
+ *
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <memory.h>
+
+extern HIST_ENTRY **history_list();
+
+int main()
+{
+    char *temp = NULL;
+    int done = 0;
+
+    while (!done)
+    {
+        temp = readline("readline$ ");
+
+        /* Test for EOF. */
+        if (!temp)
+        {
+            exit(1);
+        }
+
+        /* If there is anything on the line, print it and remember it. */
+        if (*temp)
+        {
+            fprintf(stderr, "%s\r\n", temp);
+            add_history(temp);
+        }
+
+        /* Check for `command' that we handle. */
+        if (strcmp(temp, "quit") == 0)
+        {
+            done = 1;
+        }
+        else if (strcmp(temp, "list") == 0)
+        {
+            HIST_ENTRY **list;
+            register int i;
+            list = history_list();
+            if (list)
+            {
+                for (i = 0; list[i]; i++)
+                {
+                    fprintf(stderr, "%d: %s\r\n", i, list[i]->line);
+                }
+            }
+        }
+
+        free(temp);
+    }
+
+    exit(0);
+}
