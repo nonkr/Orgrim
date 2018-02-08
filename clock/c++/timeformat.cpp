de2554651b0b4c4c657b70d218a2c7c5b86baa82
nonkr <nonkr@hotmail.com>
2018-02-08 14:45:54 +0800

    add example about how to format time_point to string

---
+++ clock/c++/timeformat.cpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2018, Billie Soong <nonkr@hotmail.com>
+ * All rights reserved.
+ *
+ * This file is under GPL, see LICENSE for details.
+ *
+ * Author: Billie Soong <nonkr@hotmail.com>
+ * Datetime: 2018/2/8 14:34
+ *
+ */
+
+#include <iostream>
+#include <chrono>
+#include <cstring>
+
+using namespace std;
+using namespace chrono;
+
+std::string time_point_to_string(std::chrono::system_clock::time_point &tp)
+{
+    using namespace std;
+    using namespace std::chrono;
+
+    auto ttime_t = system_clock::to_time_t(tp);
+    auto tp_sec = system_clock::from_time_t(ttime_t);
+    milliseconds ms = duration_cast<milliseconds>(tp - tp_sec);
+
+    std::tm * ttm = localtime(&ttime_t);
+
+    char date_time_format[] = "%Y.%m.%d-%H.%M.%S";
+
+    char time_str[] = "yyyy.mm.dd.HH-MM.SS.fff";
+
+    strftime(time_str, strlen(time_str), date_time_format, ttm);
+
+    string result(time_str);
+    result.append(".");
+    result.append(to_string(ms.count()));
+
+    return result;
+}
+
+int main()
+{
+    auto start = system_clock::now();
+
+    string s = time_point_to_string(start);
+
+    std::cout << s << std::endl;
+}
