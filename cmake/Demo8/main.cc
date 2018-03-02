3b2a8609cd0d2d4be1bed860282a612f19745f90
nonkr <nonkr@hotmail.com>
2018-03-02 11:29:09 +0800

    增加Demo_VERSION_PATCH

--- cmake/Demo8/main.cc
+++ cmake/Demo8/main.cc
@@ -8,14 +8,21 @@
   #include <MathFunctions.h>
 #endif
 
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+
+const char *VERSION = "version: v" STR(Demo_VERSION_MAJOR) "." STR(Demo_VERSION_MINOR) "." STR(Demo_VERSION_PATCH);
+
 int main(int argc, char *argv[])
 {
-    if (argc < 3){
+    if (argc < 3)
+    {
         // print version info
-        printf("%s Version %d.%d\n",
-            argv[0],
-            Demo_VERSION_MAJOR,
-            Demo_VERSION_MINOR);
+        printf("%s Version %d.%d.%d\n",
+               argv[0],
+               Demo_VERSION_MAJOR,
+               Demo_VERSION_MINOR,
+               Demo_VERSION_PATCH);
         printf("Usage: %s base exponent \n", argv[0]);
         return 1;
     }
