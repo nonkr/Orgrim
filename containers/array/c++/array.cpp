e9105044787327abcad85c8fe05b34fdd973163d
nonkr <nonkr@hotmail.com>
2018-03-09 16:11:52 +0800

    add std::array std::list examples

---
+++ containers/array/c++/array.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2018, Billie Soong <nonkr@hotmail.com>
+ * All rights reserved.
+ *
+ * This file is under MIT, see LICENSE for details.
+ *
+ * Author: Billie Soong <nonkr@hotmail.com>
+ * Datetime: 2018/3/9 15:52
+ *
+ */
+
+#include <iostream>
+#include <array>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    array<string, 5> arr = {"C++", "Java", "PHP"};
+
+    cout << "arr.size() = " << arr.size() << endl;
+
+    if (!arr.empty())
+    {
+        cout << "arr is not empty" << endl;
+    }
+
+    string lang = arr.at(0);
+    cout << "arr.at(0) = " << lang << endl;
+
+    lang = arr.at(1);
+    cout << "arr.at(1) = " << lang << endl;
+
+    lang = arr.at(2);
+    cout << "arr.at(2) = " << lang << endl;
+
+    lang = arr.at(3);
+    cout << "arr.at(3) = " << lang << endl;
+
+    lang = arr.at(4);
+    cout << "arr.at(4) = " << lang << endl;
+
+    cout << "========" << endl;
+
+    string *pStr = arr.data();
+    for (size_t n = 0; n < arr.size(); n++, pStr++)
+        cout << "arr.data() = " << *pStr << endl;
+
+    return 0;
+}
---
+++ containers/list/c++/list.cpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2018, Billie Soong <nonkr@hotmail.com>
+ * All rights reserved.
+ *
+ * This file is under MIT, see LICENSE for details.
+ *
+ * Author: Billie Soong <nonkr@hotmail.com>
+ * Datetime: 2018/3/9 15:11
+ *
+ */
+
+#include <string>
+#include <list>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void PrintIt(string &StringToPrint)
+{
+    cout << StringToPrint << endl;
+}
+
+int main()
+{
+    list<string> lst;
+
+    if (lst.empty())
+    {
+        cout << "empty list" << endl;
+    }
+
+    lst.push_back("b");
+    lst.push_back("c");
+    lst.push_front("a");
+
+    cout << "list size: " << lst.size() << endl;
+    cout << "list max_size: " << lst.max_size() << endl;
+
+    cout << "========" << endl;
+    // 遍历方式一
+    for (auto n : lst)
+    {
+        cout << n << endl;
+    }
+    cout << "========" << endl;
+
+    // 遍历方式二
+    list<string>::iterator lstIterator;
+    for (lstIterator = lst.begin(); lstIterator != lst.end(); lstIterator++)
+    {
+        cout << *lstIterator << endl;
+    }
+    cout << "========" << endl;
+
+    // 把list的元素倒转
+    lst.reverse();
+
+    // 遍历方式三
+    for_each(lst.begin(), lst.end(), PrintIt);
+    cout << "========" << endl;
+
+
+    list<int> Scores;
+    Scores.push_back(15);
+    Scores.push_back(100);
+    Scores.push_back(5);
+    Scores.push_back(25);
+    Scores.push_back(10);
+    Scores.push_back(100);
+    Scores.push_back(25);
+    Scores.push_back(30);
+    Scores.push_back(20);
+    cout << count(Scores.begin(), Scores.end(), 100) << endl;
+
+    cout << count_if(Scores.begin(), Scores.end(), [](int i) { return i % 10 == 0; }) << endl;
+
+    cout << "========" << endl;
+    for (auto n : Scores)
+    {
+        cout << n << endl;
+    }
+
+    Scores.sort();
+    cout << "==== after sort() ====" << endl;
+    for (auto n : Scores)
+    {
+        cout << n << endl;
+    }
+
+    Scores.erase(Scores.begin());
+    cout << "==== after erase(Scores.begin()) ====" << endl;
+    for (auto n : Scores)
+    {
+        cout << n << endl;
+    }
+    cout << "========" << endl;
+
+    Scores.remove(25);
+    cout << "==== after remove(25) ====" << endl;
+    for (auto n : Scores)
+    {
+        cout << n << endl;
+    }
+    cout << "========" << endl;
+
+    Scores.unique();
+    cout << "==== after unique() ====" << endl;
+    for (auto n : Scores)
+    {
+        cout << n << endl;
+    }
+    cout << "========" << endl;
+
+    return 0;
+}
