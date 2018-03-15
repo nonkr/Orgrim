b52b8790c56e7d3c849d7774e8eca7388cf9d914
nonkr <nonkr@hotmail.com>
2018-03-15 15:50:47 +0800

    add SO_REUSEADDR before socket bind

--- ipc/socket/c/simple_server.c
+++ ipc/socket/c/simple_server.c
@@ -25,9 +25,18 @@ int main()
     //将套接字和IP、端口绑定
     struct sockaddr_in serv_addr;
     memset(&serv_addr, 0, sizeof(serv_addr));  //每个字节都用0填充
-    serv_addr.sin_family = AF_INET;  //使用IPv4地址
+    serv_addr.sin_family      = AF_INET;  //使用IPv4地址
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");  //具体的IP地址
-    serv_addr.sin_port = htons(1234);  //端口
+    serv_addr.sin_port        = htons(1234);  //端口
+
+    // 设置套接字选项避免地址使用错误
+    int on                            = 1;
+    if ((setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on))) < 0)
+    {
+        perror("setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+
     bind(serv_sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
 
     //进入监听状态，等待用户发起请求
@@ -35,8 +44,8 @@ int main()
 
     //接收客户端请求
     struct sockaddr_in clnt_addr;
-    socklen_t clnt_addr_size = sizeof(clnt_addr);
-    int clnt_sock = accept(serv_sock, (struct sockaddr *) &clnt_addr, &clnt_addr_size);
+    socklen_t          clnt_addr_size = sizeof(clnt_addr);
+    int                clnt_sock      = accept(serv_sock, (struct sockaddr *) &clnt_addr, &clnt_addr_size);
 
     //向客户端发送数据
     char str[] = "Hello World!";
