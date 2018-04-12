80ac5bdf8e204fa14767267fd208e45725a6ec2f
nonkr <nonkr@hotmail.com>
2018-04-12 18:52:00 +0800

    怎样简单使用gtest

---
+++ tools/gtest/sample1/sample.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2018, Billie Soong <nonkr@hotmail.com>
+ * All rights reserved.
+ *
+ * This file is under MIT, see LICENSE for details.
+ *
+ * Author: Billie Soong <nonkr@hotmail.com>
+ * Datetime: 2018/4/12 16:58
+ *
+ */
+
+#ifndef ORGRIM_SAMPLE_H
+#define ORGRIM_SAMPLE_H
+
+int fun(int a, int b);
+
+#endif //ORGRIM_SAMPLE_H
