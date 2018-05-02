fc3357251ba21fd9a0eafa179fe898908900cb56
nonkr <nonkr@hotmail.com>
2018-05-02 17:22:06 +0800

    add scandir sample

---
+++ directory/c/scandir.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2018, Billie Soong <nonkr@hotmail.com>
+ * All rights reserved.
+ *
+ * This file is under MIT, see LICENSE for details.
+ *
+ * Author: Billie Soong <nonkr@hotmail.com>
+ * Datetime: 2018/5/2 17:03
+ *
+ */
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+    struct dirent **namelist;
+    int           n;
+
+    n = scandir(argv[1], &namelist, 0, versionsort);
+    if (n < 0)
+        perror("scandir");
+    else
+    {
+        while (n--)
+        {
+            if (!strcmp(namelist[n]->d_name, ".") || !strcmp(namelist[n]->d_name, ".."))
+            {
+                continue;
+            }
+            printf("%s\n", namelist[n]->d_name);
+            free(namelist[n]);
+        }
+        free(namelist);
+    }
+}
