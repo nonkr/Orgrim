0fcab8340ffbae168ae3c27c4348b4f7e1ecc035
nonkr <nonkr@hotmail.com>
2018-06-07 09:49:02 +0800

    add some md5 use sample

---
+++ encrypt/md5/c/md5_daemon.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2017, Billie Soong <nonkr@hotmail.com>
+ * All rights reserved.
+ *
+ * This file is under MIT, see LICENSE for details.
+ *
+ * Author: Billie Soong <nonkr@hotmail.com>
+ * Datetime: 2018/6/7 9:39
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <openssl/md5.h>
+
+int main()
+{
+    unsigned char digest1[16];
+    unsigned char digest2[16];
+    short         a[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    short         b[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    MD5((unsigned char *) a, sizeof(a), digest1);
+    MD5((unsigned char *) b, sizeof(b), digest2);
+
+    char md5string1[33];
+    char md5string2[33];
+
+    for (int i = 0; i < 16; ++i)
+    {
+        sprintf(&md5string1[i * 2], "%02x", (unsigned int) digest1[i]);
+        sprintf(&md5string2[i * 2], "%02x", (unsigned int) digest2[i]);
+    }
+
+    if (memcmp(md5string1, md5string2, sizeof(md5string1)) == 0)
+    {
+        printf("same\n");
+    }
+
+    printf("%s\n", md5string1);
+    printf("%s\n", md5string2);
+
+    return 0;
+}
