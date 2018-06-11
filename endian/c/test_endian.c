46ca4fbe5b6a9b798dbaf36e10f42600e463e32e
nonkr <nonkr@hotmail.com>
2018-06-11 10:10:47 +0800

    add htonf and ntohf to test_endian

--- endian/c/test_endian.c
+++ endian/c/test_endian.c
@@ -13,7 +13,7 @@
 #include <netinet/in.h>
 #include "../../utils/print_utils.h"
 
-static float float_swap(float value)
+static float htonf(float value)
 {
     union v
     {
@@ -25,6 +25,22 @@ static float float_swap(float value)
 
     val.f = value;
     val.i = htonl(val.i);
+
+    return val.f;
+};
+
+static float ntohf(float value)
+{
+    union v
+    {
+        float        f;
+        unsigned int i;
+    };
+
+    union v val;
+
+    val.f = value;
+    val.i = ntohl(val.i);
 
     return val.f;
 };
@@ -51,11 +67,21 @@ int main()
     float f = 1.2345;
     print_as_hexstring((char *) &f, sizeof(f));
 
-    float f2 = float_swap(f);
+    float f2 = htonf(f);
     print_as_hexstring((char *) &f2, sizeof(f2));
 
-    float f3 = float_swap(f2);
+    float f3 = ntohf(f2);
     print_as_hexstring((char *) &f3, sizeof(f3));
+
+    unsigned short s = 2;
+    print_as_hexstring((char *) &s, sizeof(s));
+
+    unsigned short s2 = htons(s);
+    print_as_hexstring((char *) &s2, sizeof(s2));
+
+    unsigned short s3 = ntohs(s2);
+    print_as_hexstring((char *) &s3, sizeof(s3));
+
 
     return 1;
 }
