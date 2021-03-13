15f37015bccd8c1f9c5e090fc95e993153a48c53
Nonkr <nonkr@hotmail.com>
2021-03-13 17:31:51 +0800

    增加cJSON_Duplicate使用说明

---
+++ file_format/json/cJSON/cJSON_Duplicate.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <cstdlib>
+#include "cJSON.h"
+
+int main(int argc, char *argv[])
+{
+    cJSON *pJSON = cJSON_CreateArray();
+    cJSON *pOneItem = cJSON_CreateObject();
+    cJSON_AddStringToObject(pOneItem, "name", "John");
+    cJSON_AddNumberToObject(pOneItem, "age", 16);
+    cJSON_AddItemToArray(pJSON, pOneItem);
+
+    char *pStr = cJSON_Print(pJSON);
+    if (pStr)
+    {
+        printf("%s\n", pStr);
+        free(pStr);
+    }
+
+    cJSON *pDup = cJSON_Duplicate(pOneItem, true);
+
+    cJSON_Delete(pJSON);
+
+    cJSON *pNewObj = cJSON_CreateObject();
+    // 将pDup加入到pNewObj后，在cJSON_Delete(pNewObj)时，会将pDup的内存也释放掉
+    cJSON_AddItemToObject(pNewObj, "swpTempArea", pDup);
+    // 所以如果后面还需要再复用pDup，需要再cJSON_Duplicate自身一遍
+    pDup = cJSON_Duplicate(pDup, true);
+    cJSON_Delete(pNewObj);
+
+    cJSON *pNewObj2 = cJSON_CreateObject();
+    cJSON_AddItemToObject(pNewObj2, "swpTempArea", pDup);
+    cJSON_Delete(pNewObj2);
+    return 0;
+}
